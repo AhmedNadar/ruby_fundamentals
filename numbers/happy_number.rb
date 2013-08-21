28b9d02089cb601c6c98d8157051c1c2d2896f20
AhmedNadar <ahmedfay@hotmail.com>
2013-08-21 17:27:38 -0400

    related keys of hashes

--- numbers/happy_number.rb
+++ numbers/happy_number.rb
@@ -5,3 +5,30 @@ def happy_number?(a)
    end
   false
 end
+
+
+def happyn_number?(number)
+	previous_number = []
+
+	# take a number - DONE
+
+	while number != 1
+		#split  into separate digits
+		digits  = number.to_s.split("")
+
+		# convert each digit to a integer and squar each digit
+		digits.map! { |digit| digit.to_i ** 2 }
+
+		# number the squares to get a new number 
+		number = digits.inject(0) {|sum, i| sum + i}
+		if previous_number.include?(number)
+			return false
+		end
+		previous_number << number
+
+		#rinse and repeeat
+	end
+	return true
+end
+
+p happyn_number?(58758764)
