2a8dcbd49794648f95e95187540d07da85b32cbd
Ahmed Nadar <ahmedfay@hotmail.com>
2014-03-02 23:30:46 -0500

    Update Fibonacci
    One liner version...

--- numbers/fibonacci.rb
+++ numbers/fibonacci.rb
@@ -2,3 +2,11 @@ def fibo_finder(n)
 	return n if (0..1).include? n
 	fibo_finder(n-1) + fibo_finder(n-2) if n > 1
 end
+
+
+# one line version
+
+def fibonacci(n)
+   n <= 1 ? n :  fibonacci( n - 1 ) + fibonacci( n - 2 ) 
+end
+puts fibonacci( 10 )
